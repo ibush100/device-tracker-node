a5412deefe77f585db1b727294b0e1afaa499912
Ian Bush <ianfracnisbush@gmail.com>
2020-06-12 10:45:32 -0400

    switching tests to mocha

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -4,12 +4,12 @@ const config = require('chai');
 const mongoose = require('mongoose');
 
 describe('user.generateAuthToken', ()=> {
-    it('should retunr a valid JWT', () => {
+    it('should return a valid JWT', () => {
     const payload = {_id: new mongoose.Types.ObjectId().toHexString()};
     const user = new User(payload);
     const token = user.generateAuthToken();
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    expect(decoded).toMachObject(payload);
+    assert.equal(decoded, payload);
     });
 
 });
